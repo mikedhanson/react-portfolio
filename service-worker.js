e286d200d398168f7479b28668af1f2f154bbdfe
mikedhanson <15201245+mikedhanson@users.noreply.github.com>
2022-12-17 02:05:43 +0000

    Deploying to gh-pages from  @ 905c1235eaa2b5daf8e1fc6d477a6454d1c35a53 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a968c2640c1e2e74ebe22caf7bfff74.js"
+  "/precache-manifest.6d1ebe08196a8a17c9162ed3b40f514f.js"
 );
 
 self.addEventListener('message', (event) => {
