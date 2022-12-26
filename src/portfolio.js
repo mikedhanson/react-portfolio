b8183fc5b1c4d515313c2db0b8c5c6a97e41a8d4
Michael Hanson <Michael.hanson1458@gmail.com>
2022-12-26 15:32:22 -0600

    Changing animation duration

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your
 const splashScreen = {
   enabled: true, // set false to disable splash screen
   animation: splashAnimation,
-  duration: 2000 // Set animation duration as per your animation
+  duration: 1000 // Set animation duration as per your animation
 };
 
 // Summary And Greeting Section
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Mike",
-  title: "<Hello World/> I'm Mike!",
+  title: "Hello World",
   subTitle: emoji("Security Enthusiast, Self-Hosting, Typical Nerd"),
   resumeLink: "https://www.linkedin.com/in/michael-h-396129134/",
   displayGreeting: true // Set false to hide this section, defaults to true
